8c7449b5406578ea63912687609bfd13fbeb1d6b
Herve2iko <ihervedelice@gmail.com>
2025-09-14 20:36:23 +0200

    add some fields per

--- api/views.py
+++ api/views.py
@@ -200,6 +200,9 @@ def receive_whatsapp_message(request):
     from_number = request.data.get("From")
     message_body = request.data.get("Body")
     to_number = request.data.get("To")
+    logger.info(request.data)
+    logger.info(request.data.get("From"))
+    logger.info(request.data.get("To"))
     msg = f"Nouveau message de {from_number}: {message_body}"
     # Tu peux sauvegarder le message dans ta base de données ici
     
